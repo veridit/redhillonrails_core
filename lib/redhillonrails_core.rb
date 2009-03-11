2043fa5b50b16aa03c71869ca84aa9fdc0e71a31
David Bourguignon <david.bourguignon@novelys.com>
2009-03-11 16:06:48 +0800

    fix compatibility with rails 2.2.2 in Threadsafe mode leading to uninitialized constant RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::AbstractAdapter::ForeignKeyDefinition
    Signed-off-by: Simon Harris <haruki_zaemon@mac.com>

--- lib/redhillonrails_core.rb
+++ lib/redhillonrails_core.rb
@@ -1,3 +1,15 @@
+require 'red_hill_consulting/core/active_record/base'
+require 'red_hill_consulting/core/active_record/connection_adapters/abstract_adapter'
+require 'red_hill_consulting/core/active_record/connection_adapters/foreign_key_definition'
+require 'red_hill_consulting/core/active_record/connection_adapters/column'
+require 'red_hill_consulting/core/active_record/connection_adapters/index_definition'
+require 'red_hill_consulting/core/active_record/connection_adapters/mysql_adapter'
+require 'red_hill_consulting/core/active_record/connection_adapters/mysql_column'
+require 'red_hill_consulting/core/active_record/connection_adapters/postgresql_adapter'
+require 'red_hill_consulting/core/active_record/connection_adapters/schema_statements'
+require 'red_hill_consulting/core/active_record/connection_adapters/sqlite3_adapter'
+require 'red_hill_consulting/core/active_record/connection_adapters/table_definition'
+
 ActiveRecord::Base.send(:include, RedHillConsulting::Core::ActiveRecord::Base)
 ActiveRecord::Schema.send(:include, RedHillConsulting::Core::ActiveRecord::Schema)
 ActiveRecord::SchemaDumper.send(:include, RedHillConsulting::Core::ActiveRecord::SchemaDumper)
